0d5a291951c686e2871e49c5fd77315b26899104
Helen Quidano <hdquidano@gmail.com>
2024-09-03 00:12:27 -0500

    Add variables in javascript and if condition to remove null value

--- src/js/app.js
+++ src/js/app.js
@@ -29,18 +29,48 @@ function render(variables = {}) {
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  let name = "";
+  if (variables.name !== null) {
+    name += variables.name + " ";
+  }
+
+  let lastName = "";
+  if (variables.lastName !== null) {
+    lastName += variables.lastName + " ";
+  }
+
+  let twitter = "";
+  if (variables.twitter !== null) {
+    twitter += variables.twitter + " ";
+  }
+
+  let github = "";
+  if (variables.github !== null) {
+    github += variables.github + " ";
+  }
+
+  let linkedin = "";
+  if (variables.linkedin !== null) {
+    linkedin += variables.linkedin + " ";
+  }
+
+  let instagram = "";
+  if (variables.instagram !== null) {
+    instagram += variables.instagram + " ";
+  }
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${name} ${lastName}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city}, ${variables.country}</h3>
+          <ul class="${variables.socialMediaPosition}">
+            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a>${twitter}</li>
+            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a>${github}</li>
+            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a>${linkedin}</li>
+            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a>${instagram}</li>
           </ul>
         </div>
     `;
