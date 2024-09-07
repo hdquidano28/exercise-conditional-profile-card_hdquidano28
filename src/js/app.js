44565eb966a90704c2628ac9c3c3bedcb24ac0af
helen.quidano <hdquidano@gmail.com>
2024-09-07 15:34:58 -0500

    Update social media variable

--- src/js/app.js
+++ src/js/app.js
@@ -68,7 +68,7 @@ function render(variables = {}) {
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.twitter}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/feed/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
